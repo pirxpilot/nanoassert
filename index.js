093dae46020fab19bc58be3c336633f85bf5209a
Damian Krzeminski <pirxpilot@furkot.com>
2022-06-28 16:39:29 +0200

    add support for DEBUG flag

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-assert.notEqual = notEqual
-assert.notOk = notOk
-assert.equal = equal
-assert.ok = assert
+module.exports = assert;
 
-module.exports = assert
+assert.notEqual = notEqual;
+assert.notOk = notOk;
+assert.equal = equal;
+assert.ok = assert;
 
-function equal (a, b, m) {
-  assert(a == b, m) // eslint-disable-line eqeqeq
+/* jshint -W116 */
+/* jshint -W030 */
+/* global DEBUG */
+
+function equal(a, b, m) {
+  DEBUG && doAssert(equal, a == b, m);
 }
 
-function notEqual (a, b, m) {
-  assert(a != b, m) // eslint-disable-line eqeqeq
+function notEqual(a, b, m) {
+  DEBUG && doAssert(notEqual, a != b, m);
 }
 
-function notOk (t, m) {
-  assert(!t, m)
+function notOk(t, m) {
+  DEBUG && doAssert(notOk, !t, m);
 }
 
-function assert (t, m) {
-  if (!t) throw new Error(m || 'AssertionError')
+function assert(t, m) {
+  DEBUG && doAssert(assert, t, m);
+}
+
+class AssertionError extends Error { }
+
+function doAssert(fn, t, m) {
+  if (!t) {
+    const err = new AssertionError(m);
+    Error.captureStackTrace?.(err, fn);
+    throw err;
+  }
 }
--- test.js
+++ test.js
@@ -1,27 +1,47 @@
-var assert = require('.')
-var test = require('tape')
+const assert = require('.');
+const test = require('tape');
 
-test('test', function (t) {
+
+test('test DEBUG', function (t) {
+  global.DEBUG = true;
+
+  t.plan(10);
+
+  t.doesNotThrow(() => assert(true === true), 'does not throw on truthy');
+  t.doesNotThrow(() => assert.notOk(false), 'does not throw on false');
+  t.doesNotThrow(() => assert.equal(1, 1), 'does not throw on equal');
+  t.doesNotThrow(() => assert.notEqual(1, 2), 'does not throw on not equal');
+
+  t.throws(() => assert(false), 'throws on falsy');
+  t.throws(() => assert.notOk(true), 'throws on truthy');
+  t.throws(() => assert.equal(1, 2), 'throws on not equal');
+  t.throws(() => assert.notEqual(1, 1), 'throws on equal');
+
   try {
-    assert(true === true) // test that it doesn't throw
-    t.pass('does not throw on truthy')
+    assert(false);
   } catch (e) {
-    t.fail()
+    t.equal(e.message, '', 'default message');
   }
 
-  t.throws(assert.bind(null, false), 'throws on falsy')
-
   try {
-    assert(false)
+    assert(false, 'hello world');
   } catch (e) {
-    t.equal(e.message, 'AssertionError', 'default message')
+    t.equal(e.message, 'hello world', 'custom message');
   }
+});
 
-  try {
-    assert(false, 'hello world')
-  } catch (e) {
-    t.equal(e.message, 'hello world', 'custom message')
-  }
+test('test NDEBUG', function (t) {
+  global.DEBUG = false;
 
-  t.end()
-})
+  t.plan(8);
+
+  t.doesNotThrow(() => assert(true === true), 'does not throw on truthy');
+  t.doesNotThrow(() => assert.notOk(false), 'does not throw on false');
+  t.doesNotThrow(() => assert.equal(1, 1), 'does not throw on equal');
+  t.doesNotThrow(() => assert.notEqual(1, 2), 'does not throw on not equal');
+
+  t.doesNotThrow(() => assert(false), 'throws on falsy');
+  t.doesNotThrow(() => assert.notOk(true), 'throws on truthy');
+  t.doesNotThrow(() => assert.equal(1, 2), 'throws on not equal');
+  t.doesNotThrow(() => assert.notEqual(1, 1), 'throws on equal');
+});
