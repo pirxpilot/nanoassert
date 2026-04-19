dd07c70648880e66359d6cf76afaedc3c2552703
Damian Krzeminski <pirxpilot@furkot.com>
2026-04-19 11:10:31 -0600

    upgrade `biome` to 2.4.11

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
 const test = require('node:test');
 
-const assert = require('.');
+const assert = require('./index.js');
 
-test('test DEBUG', function (t) {
-  global.DEBUG = true;
-
+test('test DEBUG', t => {
   t.plan(10);
 
-  // biome-ignore lint/suspicious/noSelfCompare: <explanation>
+  // biome-ignore lint/suspicious/noSelfCompare: needed for testing purposes
   t.assert.doesNotThrow(() => assert(true === true), 'does not throw on truthy');
   t.assert.doesNotThrow(() => assert.notOk(false), 'does not throw on false');
   t.assert.doesNotThrow(() => assert.equal(1, 1), 'does not throw on equal');
@@ -31,12 +29,12 @@ test('test DEBUG', function (t) {
   }
 });
 
-test('test NDEBUG', function (t) {
+test('test NDEBUG', t => {
   global.DEBUG = false;
 
   t.plan(8);
 
-  // biome-ignore lint/suspicious/noSelfCompare: <explanation>
+  // biome-ignore lint/suspicious/noSelfCompare: needed for testing purposes
   t.assert.doesNotThrow(() => assert(true === true), 'does not throw on truthy');
   t.assert.doesNotThrow(() => assert.notOk(false), 'does not throw on false');
   t.assert.doesNotThrow(() => assert.equal(1, 1), 'does not throw on equal');
