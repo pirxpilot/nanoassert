47aee0d90fc9686b50e37250153ebe2045f2924c
Damian Krzeminski <pirxpilot@furkot.com>
2026-04-19 11:28:15 -0600

    remove DEBUG flag

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ assert.equal = equal;
 assert.ok = assert;
 
 function equal(a, b, m) {
-  // biome-ignore lint/suspicious/noDoubleEquals: <explanation>
-  DEBUG && doAssert(equal, a == b, m);
+  // biome-ignore lint/suspicious/noDoubleEquals: non-strict equality is intentional here
+  doAssert(equal, a == b, m);
 }
 
 function notEqual(a, b, m) {
-  // biome-ignore lint/suspicious/noDoubleEquals: <explanation>
-  DEBUG && doAssert(notEqual, a != b, m);
+  // biome-ignore lint/suspicious/noDoubleEquals: non-strict inequality is intentional here
+  doAssert(notEqual, a != b, m);
 }
 
 function notOk(t, m) {
-  DEBUG && doAssert(notOk, !t, m);
+  doAssert(notOk, !t, m);
 }
 
 function assert(t, m) {
-  DEBUG && doAssert(assert, t, m);
+  doAssert(assert, t, m);
 }
 
 class AssertionError extends Error {}
--- test.js
+++ test.js
@@ -28,20 +28,3 @@ test('test DEBUG', t => {
     t.assert.equal(e.message, 'hello world', 'custom message');
   }
 });
-
-test('test NDEBUG', t => {
-  global.DEBUG = false;
-
-  t.plan(8);
-
-  // biome-ignore lint/suspicious/noSelfCompare: needed for testing purposes
-  t.assert.doesNotThrow(() => assert(true === true), 'does not throw on truthy');
-  t.assert.doesNotThrow(() => assert.notOk(false), 'does not throw on false');
-  t.assert.doesNotThrow(() => assert.equal(1, 1), 'does not throw on equal');
-  t.assert.doesNotThrow(() => assert.notEqual(1, 2), 'does not throw on not equal');
-
-  t.assert.doesNotThrow(() => assert(false), 'throws on falsy');
-  t.assert.doesNotThrow(() => assert.notOk(true), 'throws on truthy');
-  t.assert.doesNotThrow(() => assert.equal(1, 2), 'throws on not equal');
-  t.assert.doesNotThrow(() => assert.notEqual(1, 1), 'throws on equal');
-});
